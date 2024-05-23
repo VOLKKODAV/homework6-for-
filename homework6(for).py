78f65c6dec41a98723db24f982ca0909346a9253
VOLKKODAV <167250805+VOLKKODAV@users.noreply.github.com>
2024-05-23 19:40:48 +0300

    Add files via upload

---
+++ homework6(for).py
@@ -0,0 +1,11 @@
+cars_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars_list:
+    print('я ездию на машине марки', i)
+print()
+car_count = 0
+for car in cars_list:
+    cars_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+    for i in cars_list:
+        print('я ездию на машине марки', i)
+    car_count += 10
+    print(car_count)
